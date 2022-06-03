d0d895de14939dd7c577b090e4225a0bd103c53b
mishra28soumya <mishra28soumya@gmail.com>
2022-06-03 20:14:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ setInterval(() => {
   min_rotation = 6 * min;
   sec_rotation = 6 * sec;
 
+  hour = document.getElementById("hour_hand");
+  minute = document.getElementById("minute_hand");
+  second = document.getElementById("second_hand");
+
   hour.style.transform = `rotate(${hr_rotation}deg)`;
   minute.style.transform = `rotate(${min_rotation}deg)`;
   second.style.transform = `rotate(${sec_rotation}deg)`;
 }, 1000);
+
